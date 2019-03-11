956536fcfc7b5bf574d5f0ffa21c0a06f9c04998
Steve <stephen.wilson@beds.ac.uk>
2019-03-11 18:06:17 +0000

    0.1
    > Basic implimentation.
    > Issue with colliding with moving surfaces.
    > Issue parenting to moving surfaces.

---
+++ VRTest/Assets/Scripts/Debug/TeleportSticking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class TeleportSticking : MonoBehaviour
+{
+    private GameObject mg_LastHit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<TeleportArea>())
+        {
+            Debug.Log("Hit new surface");
+            if (mg_LastHit)
+            {
+                if(mg_LastHit != other.gameObject)
+                {
+                    //Debug.Log("Hit new surface");
+                    //transform.SetParent(other.transform);
+                }
+            }
+            else
+            {
+                mg_LastHit = other.gameObject;
+            }
+        }
+    }
+}
